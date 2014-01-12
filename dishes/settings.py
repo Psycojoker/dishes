2005a8c95f854c4cb483f5ddf65e07fa66d75139
Laurent Peuch <cortex@worlddomination.be>
2014-01-12 04:09:01 +0100

    [enh] install django-extensions

--- dishes/settings.py
+++ dishes/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.django_app.default',
+    'django_extensions',
     'base',
     'formulas',
 )
